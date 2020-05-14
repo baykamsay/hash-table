a24d8ad20c3d8d9002bb365acd15776d8cb896cf
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-05-14 18:41:43 +0300

    Added insert

--- hash-table/HashTable.cpp
+++ hash-table/HashTable.cpp
@@ -12,25 +12,20 @@ using namespace std;
 HashTable::HashTable(const int tableSize, const CollisionStrategy option) {
 	size = tableSize;
 	cs = option;
-	table = new int*[size];
-	for (int i = 0; i < size; i++) {
-		table[i] = NULL;
-	}
+	table = new Integer[size];
 }
 
 HashTable::~HashTable() {
-	for (int i = 0; i < size; i++) {
-		delete table[i];
-		table[i] = NULL;
-	}
 	delete[] table;
 	table = NULL;
 }
 
+// key mod table size
 int HashTable::hash(int key) {
 	return key % size;
 }
 
+// reverse the key
 int HashTable::hash2(int key) {
 	int result = 0;
 	bool neg = false;
@@ -52,6 +47,7 @@ int HashTable::hash2(int key) {
 	return result;
 }
 
+// choose between different collision strategies
 int HashTable::f(int i, int key) {
 	int result;
 
@@ -71,4 +67,42 @@ int HashTable::f(int i, int key) {
 		break;
 	}
 	return result;
+}
+
+int HashTable::h_i(int key, int i) {
+	int result = (hash(key) + f(i, key)) % size;
+	if (result < 0)
+		result *= -1;
+	return result;
+}
+
+int HashTable::findLocation(int key) {
+	int firstLoc = h_i(key, 0);
+	if (table[firstLoc].isEmpty)
+		return firstLoc;
+
+	for (int i = 1; true; i++) {
+		int loc = h_i(key, i);
+		if (loc == firstLoc)
+			return -1;
+
+		if (table[loc].isEmpty) {
+			return loc;
+		}
+	}
+}
+
+bool HashTable::insert(const int item) {
+	int loc = findLocation(item);
+
+	if (loc == -1)
+		return false;
+	
+	table[loc].data = item;
+	table[loc].isEmpty = false;
+	return true;
+}
+
+bool HashTable::remove(const int item) {
+
 }
--- hash-table/HashTable.h
+++ hash-table/HashTable.h
@@ -9,6 +9,10 @@
 #define __HASHTABLE_H
 
 enum CollisionStrategy {LINEAR, QUADRATIC, DOUBLE};
+struct Integer {
+	int data = 0;
+	bool isEmpty = true;
+};
 
 class HashTable {
 public:
@@ -22,10 +26,12 @@ public:
 private:
 	int hash(int key); // hash function (mod in this case)
 	int hash2(int key); // second hash function for double hashing
-	int f(int i, int key); // collusion resolution strategy
+	int f(int i, int key); // collision resolution strategy
+	int h_i(int key, int i); // as seen in hw4 pdf
+	int findLocation(int key);
 
 	CollisionStrategy cs;
 	int size;
-	int** table;
+	Integer* table;
 };
 #endif // !__HASHTABLE_H
